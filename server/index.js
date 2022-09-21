aaf043da843b031d1e940ce320dd8727305efa25
lanemunds <lanemunds@gmail.com>
2022-09-21 15:05:20 -0600

    addedA

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
 require('dotenv').config()
-const port = process.env.PORT 
+const port = process.env.PORT ||5001
 const app = express()
-app.request(express.json())
-pp.use("/", express.static(path.join(__dirname,'...client/index.html')))
+app.use(express.json())
+
+app.use("/", express.static(path.join(__dirname,'...client/index.html')))
 app.use(express.static(path.join(__dirname,('.../client'))))
 
 
