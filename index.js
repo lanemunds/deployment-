f5c5959a05b3f94a540f372ab0a1e82392d2b2c4
lanemunds <lanemunds@gmail.com>
2022-09-21 14:40:09 -0600

    addedPath

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ require('dotenv').config()
 const port = process.env.PORT 
 const app = express()
 app.request(express.json())
-
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
 
 app.listen(port, ()=>{
     console.log('docked at port '+ port)
