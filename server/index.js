bf51adc0e0e075949558c83ef47395d11f021b83
lanemunds <lanemunds@gmail.com>
2022-09-21 15:12:31 -0600

    fixes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const path = require('path')
 require('dotenv').config()
-const port = process.env.PORT ||5001
+const port = process.env.PORT ||5000
 const app = express()
 app.use(express.json())
 
-app.use("/", express.static(path.join(__dirname,'...client/index.html')))
-app.use(express.static(path.join(__dirname,('.../client'))))
+app.use('/', express.static(path.join(__dirname,'..client/index.html')))
+app.use(express.static(path.join(__dirname,('../client'))))
 
 
 
