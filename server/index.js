0dd90ee358e91433b1b62c559a7da20c3e825516
lanemunds <lanemunds@gmail.com>
2022-09-21 14:58:54 -0600

    third

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ require('dotenv').config()
 const port = process.env.PORT 
 const app = express()
 app.request(express.json())
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, '/index.html'))
-})
+pp.use("/", express.static(path.join(__dirname,'...client/index.html')))
+app.use(express.static(path.join(__dirname,('.../client'))))
+
+
 
 app.listen(port, ()=>{
     console.log('docked at port '+ port)
