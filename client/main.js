18e12354bc4f95915568c71aa62e367b8df08ece
lanemunds <lanemunds@gmail.com>
2022-09-21 16:50:47 -0600

    button

--- client/main.js
+++ client/main.js
@@ -1,1 +1,10 @@
 console.log('hello World')
+
+const contactUs = (event)=>{
+    event.preventDefault()
+    alert("Please don't contact us")
+}
+
+const contact = document.querySelector('#contact')
+
+contact.addEventListener('click',contactUs)
