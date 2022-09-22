44957abe871b4283b0b47b22dc23b27224a1f95b
lanemunds <lanemunds@gmail.com>
2022-09-22 15:11:44 -0600

    rollbarAdd

--- client/main.js
+++ client/main.js
@@ -1,10 +1,21 @@
+
+const rollbar = require('rollbar')
+
 console.log('hello World')
 
 const contactUs = (event)=>{
     event.preventDefault()
     alert("Please don't contact us")
+    rollbar.log();('hello')
+const buyNow = (event)=>{
+    event.preventDefault()
+    alert("Please don't contact us")
+    rollbar.log();('hello')
 }
 
 const contact = document.querySelector('#contact')
+const buy = document.querySelector('#buy')
 
 contact.addEventListener('click',contactUs)
+buy.addEventListener('click',buyNow)
+
--- server/index.js
+++ server/index.js
@@ -8,7 +8,23 @@ app.use(express.json())
 app.use('/', express.static(path.join(__dirname,'..client/index.html')))
 app.use(express.static(path.join(__dirname,('../client'))))
 
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '774d489357f949608451bc855c7edafd',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
 
+rollbar.log("Hello world!");
+
+try {
+    nonExistentFunction();
+  } catch (error) {
+    rollbar.error(error)};
+try {
+    nonExistentFunction2();
+  } catch (error) {
+    rollbar.critical(error)};
 
 app.listen(port, ()=>{
     console.log('docked at port '+ port)
